de9f45388c96a886e34f18cde816e43184bb6e79
Josh Murunga <146818602+Josh-Murunga@users.noreply.github.com>
2026-04-01 13:00:44 +0300

    Add renaming for dispensing app distribution folder

--- dha_build.sh
+++ dha_build.sh
@@ -96,6 +96,7 @@ rename_dist_folder() {
 # Handle renaming for openmrs-esm-form-entry-app-*
 rename_dist_folder "openmrs-esm-form-entry-app-*" "dist-form-entry"
 rename_dist_folder "openmrs-esm-service-queues-app-*" "dist-service-queues"
+rename_dist_folder "openmrs-esm-dispensing-app-*" "dist-dispense-app"
 
 # Exit with success status
 exit 0
